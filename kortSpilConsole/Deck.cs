6e5d297c28a5c3a005b34dd245eb741438a87103
christianfrom <43375010+christianfrom@users.noreply.github.com>
2018-10-11 09:36:07 +0200

    fixede nogle ting

--- kortSpilConsole/Deck.cs
+++ kortSpilConsole/Deck.cs
@@ -9,13 +9,13 @@ namespace kortSpilConsole
     class Deck
     {
         private List<Card> cards = new List<Card>();
-        private List<Card> cardsRevealed = new List<Card>();
+        public List<Card> cardsRevealed = new List<Card>();
         private UnoGame game;
 
         public Deck(UnoGame game)
         {
             this.game = game;
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 1; i++)
             {
                 // red cards
                 cards.Add(new Card("red", ""+i));
@@ -31,11 +31,27 @@ namespace kortSpilConsole
                 cards.Add(new Card("yellow", ""+i));
 
             }
-
-            for (int i = 0; i < 3; i++)
+            //sorte kort
+            for (int i = 0; i < 40; i++)
             {
+                cards.Add(new Card("black", "+4"));
+                cards.Add(new Card("black", "switch color"));
+            }
+            //reverse og skip og plus 2
+            for (int i = 0; i < 2; i++)
+            {
+                cards.Add(new Card("red", "reverse"));
+                cards.Add(new Card("blue", "reverse"));
+                cards.Add(new Card("green", "reverse"));
+                cards.Add(new Card("yellow", "reverse"));
+                cards.Add(new Card("red", "skip"));
+                cards.Add(new Card("blue", "skip"));
+                cards.Add(new Card("green", "skip"));
+                cards.Add(new Card("yellow", "skip"));
                 cards.Add(new Card("red", "+2"));
-                cards.Add(new Card("black", "skip"));
+                cards.Add(new Card("blue", "+2"));
+                cards.Add(new Card("green", "+2"));
+                cards.Add(new Card("yellow", "+2"));
             }
 
             Shuffle();
@@ -74,12 +90,43 @@ namespace kortSpilConsole
 
         public bool playCard(Card card)
         {
-            if (Peek().Color == card.Color || Peek().Value == card.Value)
+            if (Peek().Color == card.Color || Peek().Value == card.Value || card.Color == "black")
             {
+                game.currentPlayer.Hand.Remove(card);
                 cardsRevealed.Add(card);
+                
+                if (Peek().Value == "skip"){ game.nextPlayer(); }
+
+                else if (Peek().Value == "reverse") { game.players.Reverse(); }
+                else if (Peek().Color == "black")
+                {
+                    Console.WriteLine("Which color would you like");
+                    Console.WriteLine("1=red 2=blue 3=green 4=yellow");
+                    string colorPick = Console.ReadLine();
+                    if (colorPick == "1") {colorPick = "red";}
+                    else if (colorPick == "2") { colorPick = "blue"; }
+                    else if (colorPick == "3") { colorPick = "green"; }
+                    else if (colorPick == "4") { colorPick = "yellow"; }
+                    Peek().Color = colorPick;
+                }
+                
+                if (Peek().Value == "+4")
+                {
+                    game.players[game.players.IndexOf(game.currentPlayer) + 1].DrawCard(4);
+                }
+                if (Peek().Value == "+2")
+                {
+                    game.players[game.players.IndexOf(game.currentPlayer) + 1].DrawCard(2);
+                }
                 return true;
             }
-            else return false;
+
+
+            else
+            {
+                game.currentPlayer.DrawCard();
+                return false;
+            }
         }
 
         
@@ -89,9 +136,9 @@ namespace kortSpilConsole
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < cards.Count; i++)
             {
-                sb.Append("[");
+                sb.Append("[ ");
                 sb.Append(cards[i]);
-                sb.Append("], ");
+                sb.Append(" ], ");
             }
 
             return sb.ToString();
@@ -99,6 +146,7 @@ namespace kortSpilConsole
 
         public Card Peek()
         {
+            
             return cardsRevealed.Last();
         }
     }
